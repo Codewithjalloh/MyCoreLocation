d0a923704f7d3df9d04f3962bb36b8bceb55922e
D.A Jalloh <info@codewithjalloh.com>
2016-06-30 00:52:52 +0100

    action function added for tap gesture

--- MyCoreLocation/ViewController.swift
+++ MyCoreLocation/ViewController.swift
@@ -10,12 +10,33 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     @IBOutlet weak var theMap: MKMapView!
+    let locationManager: CLLocationManager = CLLocationManager()
+    var myLatitude: CLLocationDegrees!
+    var myLongitude: CLLocationDegrees!
+    var finalLatitude: CLLocationDegrees!
+    var finalLongtitude: CLLocationDegrees!
+    var distance: CLLocationDistance!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        let tap = UITapGestureRecognizer(target: self, action: "actionTap:")
+        
+        theMap.addGestureRecognizer(tap)
+    }
+    
+    func actionTap(gesturRecogniser: UITapGestureRecognizer) {
+        
     }
 
     override func didReceiveMemoryWarning() {
